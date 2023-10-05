5411c201c2c71207ec7dbfe3504c43b42b65bbbb
tusharchaudhari30 <chaudharitushar2077@gmail.com>
2023-10-06 01:16:21 +0530

    logs added for moviebooking service

--- src/main/java/com/moviebooking/moviebookingapp/service/MovieBookingService.java
+++ src/main/java/com/moviebooking/moviebookingapp/service/MovieBookingService.java
@@ -1,5 +1,7 @@
 package com.moviebooking.moviebookingapp.service;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -16,6 +18,7 @@ import com.moviebooking.moviebookingapp.repository.MovieRepository;
 import com.moviebooking.moviebookingapp.repository.TicketRepository;
 
 @Service
+@Slf4j
 public class MovieBookingService {
 
     @Autowired
@@ -28,50 +31,60 @@ public class MovieBookingService {
     AuthenticationService authenticationService;
 
     public List<Movie> viewAllMovies() {
+        log.info("Fetching all movies.");
         return movieRepository.findAll();
     }
 
     public List<Movie> searchMoviesByName(String name) {
+        log.info("Searching for movies by name: {}", name);
         return movieRepository.findByNameContainingIgnoreCase(name);
     }
 
     public ToastMessage bookTicketsForMovie(String movieId, Ticket ticket, String token) {
         User user = authenticationService.validate(token);
         if (Objects.isNull(ticket.getNumberOfTickets()) || ticket.getNumberOfTickets() == 0) {
+            log.error("Invalid number of tickets provided.");
             throw new UserCustomException("Please Enter Number of tickets");
         }
         Optional<Movie> movie = movieRepository.findById(movieId);
         ticket.setUserId(user.getId());
         if (movie.isEmpty()) {
+            log.error("Movie not found with ID: {}", movieId);
             throw new UserCustomException("Movie not Found :" + movieId);
         }
         ticket.setTheatreName(movie.get().getName());
         ticket.setMovieId(movie.get().getId());
         ticketRepository.save(ticket);
+        log.info("Ticket booked successfully for movie: {}", movie.get().getName());
         return new ToastMessage("Ticket Booked Successfully.");
     }
 
     public ToastMessage updateTicketStatus(String movieId, Integer ticket, String token) {
         User user = authenticationService.validate(token);
         if (!"Admin".equals(user.getType())) {
+            log.error("User is not admin. Access denied.");
             throw new UserCustomException("User is not admin");
         }
         Optional<Movie> movie = movieRepository.findById(movieId);
         if (movie.isEmpty()) {
+            log.error("Movie not found with ID: {}", movieId);
             throw new UserCustomException("Movie not Found :" + movieId);
         }
         Movie movie2 = movie.get();
         movie2.setTotalTicket(ticket);
         movieRepository.save(movie2);
+        log.info("Ticket details updated for movie: {}", movie2.getName());
         return new ToastMessage("Ticket Details updated.");
     }
 
     public ToastMessage deleteMovie(String id, String token) {
         User user = authenticationService.validate(token);
         if (!"Admin".equals(user.getType())) {
+            log.error("User is not admin. Access denied.");
             throw new UserCustomException("User is not admin");
         }
         movieRepository.deleteById(id);
+        log.info("Movie deleted with ID: {}", id);
         return new ToastMessage("Movie deleted successfully.");
     }
 }
